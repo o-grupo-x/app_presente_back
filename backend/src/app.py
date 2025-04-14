11ba4ab0c16bceaf310b8357a1a44eb322c493e6
Guilherme Narde da Lapa <wanderfull.mystc@gmail.com>
2025-04-13 23:26:56 -0300

    added health parameter

--- backend/src/app.py
+++ backend/src/app.py
@@ -4,5 +4,8 @@ from utils import oidc
 
 app = create_app('settings.py')
 oidc.init_app(app)
+@app.route('/health')
+def health():
+    return jsonify({"status": "ok"}), 200
 
 serve(app, host="0.0.0.0", port=8000)
