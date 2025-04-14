de24ffeadb2623d2cd5bf831b97f80b3f6334409
Guilherme Narde da Lapa <wanderfull.mystc@gmail.com>
2025-04-14 00:15:42 -0300

    added health parameter

--- backend/src/app.py
+++ backend/src/app.py
@@ -1,10 +1,12 @@
 from application import create_app
+from flask import jsonify  # Add this import
 from waitress import serve
 from utils import oidc
 
 app = create_app('settings.py')
 oidc.init_app(app)
-@app.route('/api/health')
+
+@app.route('/health')  # Use /health for HEALTHCHECK
 def health():
     return jsonify({"status": "ok"}), 200
 
