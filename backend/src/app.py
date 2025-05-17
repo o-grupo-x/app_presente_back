28073a322c7665154c8d0650b5f2888069790b9f
Gabriel Kuhnen <102557227+Kript0n007@users.noreply.github.com>
2025-05-16 23:55:15 -0300

    Update app.py

--- backend/src/app.py
+++ backend/src/app.py
@@ -8,6 +8,8 @@ oidc.init_app(app)
 
 @app.route('/api/health')
 def health():
-    return jsonify({"status": "okk production"}), 200
+    return jsonify({"status": "okk ta na production"}), 200
+
+print("🚀 Versão de teste de deploy: v1.0.3")
 
 serve(app, host="0.0.0.0", port=8000)
